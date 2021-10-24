957ca2d1ef1e3388f7fc3af36471446ccef12956
wuyepabdul <wuyepabdul@gmail.com>
2021-10-24 19:02:42 +0100

    Add CompetitionModel and PostModel

--- backend/models/CompetitonModel.js
+++ backend/models/CompetitonModel.js
@@ -1,14 +1,18 @@
 import mongoose from 'mongoose';
 
-const competionSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  numberOfTeams: { type: Number, required: true },
-  numOfTeamPlayers: { type: Number, required: true },
-  numOfYellowCardsToDisqualify: { type: Number, required: true },
-  startDate: { type: String, required: true },
-  endDate: { type: String, required: true },
-  status: { type: String, default: 'Not Started' },
-});
+const competionSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    numberOfTeams: { type: Number, required: true },
+    numOfTeamPlayers: { type: Number, required: true },
+    numOfYellowCardsToDisqualify: { type: Number, required: true },
+    startDate: { type: String, required: true },
+    endDate: { type: String, required: true },
+    reports: { type: String },
+    status: { type: String, default: 'Not Started' },
+  },
+  { timestamps: true }
+);
 
 const Competition = mongoose.model('Competition', competionSchema);
 export default Competition;
