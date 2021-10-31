1d55e3f26aa817703382674f109f1277cb9f68eb
wuyepabdul <wuyepabdul@gmail.com>
2021-10-31 12:29:07 +0100

    Add update and delete team controlllers and routes

--- backend/controllers/competitionTeamsController.js
+++ backend/controllers/competitionTeamsController.js
@@ -21,8 +21,8 @@ export const registerTeamController = asyncHandler(async (req, res) => {
 export const getAllTeamsController = asyncHandler(async (req, res) => {
   try {
     const teams = await CompetitionTeam.find({});
-    if (teams.length ===0) {
-      res.json({message:'No teams registered yet'});
+    if (teams.length === 0) {
+      res.json({ message: 'No teams registered yet' });
     } else {
       res.json(teams);
     }
@@ -33,15 +33,62 @@ export const getAllTeamsController = asyncHandler(async (req, res) => {
 });
 
 export const getTeamController = asyncHandler(async (req, res) => {
+  try {
+    const team = await CompetitionTeam.findById(req.params.id);
+    if (team) {
+      res.json(team);
+    } else {
+      res.status(404).json({ message: 'Team not found' });
+    }
+  } catch (error) {
+    console.log('error', error.message);
+    res.status(500).json({ message: 'Server error, try again later' });
+  }
+});
+
+export const updateTeamController = asyncHandler(async (req, res) => {
+  try {
+    const { teamName, teamColors, coach, competition, playerInfo } = req.body;
+    const team = await CompetitionTeam.findById(req.params.id);
+    if (team) {
+      team.teamName = teamName || team.teamName;
+      team.teamColors = teamColors || team.teamColors;
+      team.coach = coach || team.coach;
+      team.competition = competition || team.competition;
+      team.players.map((player) => {
+        player.name = playerInfo.name || player.name;
+        player.position = playerInfo.position || player.position;
+        player.jerseyNumber = playerInfo.jerseyNumber || player.jerseyNumber;
+        player.department = playerInfo.department || player.department;
+        player.faculty = playerInfo.faculty || player.faculty;
+        player.localGovArea = playerInfo.localGovArea || player.localGovArea;
+      });
+    } else {
+      res.status(404).json({ message: 'Team not found' });
+    }
+    await team.save();
+    res.json({ message: 'Team Info updated successfully', team });
+  } catch (error) {
+    console.log('error', error.message);
+    res.status(500).json({ message: 'Server error, try again later' });
+  }
+});
+
+export const deleteCompetitionTeamController = asyncHandler(
+  async (req, res) => {
     try {
-      const team = await CompetitionTeam.findById(req.params.id);
-      if (team) {
-        res.json(team);
+      const deletedTeam = await CompetitionTeam.findOneAndDelete({
+        _id: req.params.id,
+      }).exec();
+      if (deletedTeam) {
+        res.json({ message: 'Team Deleted', deletedTeam });
       } else {
-        res.status(404).json({ message: 'Team not found' });
+        res.status(404).json('Team not found');
       }
     } catch (error) {
       console.log('error', error.message);
-      res.status(500).json({ message: 'Server error, try again later' });
+      res.status(500).json({ message: 'Server error' });
     }
-  });
+  }
+);
+
--- backend/routes/competitionTeamsRoute.js
+++ backend/routes/competitionTeamsRoute.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import {
+  deleteCompetitionTeamController,
   getAllTeamsController,
   getTeamController,
   registerTeamController,
+  updateTeamController,
 } from '../controllers/competitionTeamsController.js';
 const router = express.Router();
 
@@ -11,5 +13,9 @@ router.post('/', registerTeamController);
 router.get('/', getAllTeamsController);
 
 router.get('/:id', getTeamController);
+
+router.put('/:id', updateTeamController);
+
+router.delete('/:id', deleteCompetitionTeamController);
 
 export default router;
