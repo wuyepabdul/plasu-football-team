8e1852ee4f40aea477292d691b6b7cbb481218f8
wuyepabdul <wuyepabdul@gmail.com>
2021-11-25 16:06:03 +0100

    Add Homome page hero section and navbar

---
+++ frontend/src/Pages/HomePage.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div className="home">
+      <div className="hero-container">
+        <div className="hero section">hero sect</div>
+        <h1 className='hero-heading'>Hero section</h1>
+      </div>
+      <div className="intro section">Intro</div>
+      <div className="team section">Plasu Team</div>
+      <div className="competitions section">Competitions</div>
+      <div className="contact section">Contact Us</div>
+    </div>
+  );
+};
+
+export default HomePage;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,6 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-
     <ChakraProvider>
     <App />
     </ChakraProvider>
